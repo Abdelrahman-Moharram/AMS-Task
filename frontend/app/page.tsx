8090bd10034ebbb9a4bc01f2310ea22bd610534a
abdelrahman moharram <Abdelrahman-Moharram>
2024-07-14 22:23:03 +0300

    add project files

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import DataTable from "@/Components/Tables/DataTable";
+
+export default function page() {
+  return (
+    <div className="lg:w-[80%] min-h-screen w-full mx-auto bg-white rounded-lg my-3 overflow-hidden ">
+      <DataTable />
+    </div>
+  );
+}
